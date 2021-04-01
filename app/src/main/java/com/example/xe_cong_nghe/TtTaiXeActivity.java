bb0fb25826ee1a30a10b34fd80f8ec2278224f8b
T1000 <viettel1954@gmail.com>
2021-04-02 00:10:52 +0700

    add ttkhachhang and tttaixe

---
+++ app/src/main/java/com/example/xe_cong_nghe/TtTaiXeActivity.java
@@ -0,0 +1,14 @@
+package com.example.xe_cong_nghe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TtTaiXeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tt_tai_xe);
+    }
+}
