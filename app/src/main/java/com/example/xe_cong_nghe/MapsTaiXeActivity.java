c1b1b66af2d6adf43c3d7832e190f255d7e407c4
T1000 <viettel1954@gmail.com>
2021-04-14 19:20:20 +0700

    test location

--- app/src/main/java/com/example/xe_cong_nghe/MapsTaiXeActivity.java
+++ app/src/main/java/com/example/xe_cong_nghe/MapsTaiXeActivity.java
@@ -1,44 +1,100 @@
 package com.example.xe_cong_nghe;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import android.location.Location;
-import android.os.Bundle;
+//import com.directions.route.AbstractRouting;
+//import com.directions.route.Route;
+//import com.directions.route.RouteException;
+//import com.directions.route.Routing;
+//import com.directions.route.RoutingListener;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Looper;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class MapsTaiXeActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
+
+public class MapsTaiXeActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap;
-    Location mLastLocation;
-    LocationRequest mLocationRequest;
+    LocationRequest locationRequest;
+    Location lastLocation;
 
-    FusedLocationProviderClient client;
+    FusedLocationProviderClient fusedLocationProviderClient;
     SupportMapFragment mapFragment;
+
+    LinearLayout tt_kh;
+    TextView name_kh, phone_kh, destination_kh;
+    ImageView image;
+
+    String customerId = "", destination;
+    LatLng destinationLatLng, pickupLatLng;
+    float rideDistance;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_tai_xe);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        client = LocationServices.getFusedLocationProviderClient(this);
-
-        //check permission
+        //
+//        tt_kh = (LinearLayout) findViewById(R.id.tt_kh);
+//        image = (ImageView) findViewById(R.id.image);
+//        name_kh = (TextView) findViewById(R.id.name_khachhang);
+//        phone_kh = (TextView) findViewById(R.id.phone_khachhang);
+//        destination_kh = (TextView) findViewById(R.id.destination_khachhang);
+//
+        //
+        //getAssignedCustomer();
 
     }
 
+    //private void getAssignedCustomer() {
+
+    //}
 
 
     /**
@@ -54,9 +110,93 @@ public class MapsTaiXeActivity extends FragmentActivity implements OnMapReadyCal
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(14.0583, 108.2772);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Viet Nam"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                //fusedLocationProviderClient.requestLocationUpdates(locationRequest, mLocationCallback, Looper.myLooper());
+                //mMap.setMyLocationEnabled(true);
+            }else{
+                checkLocationPermission();
+            }
+        }
+
+        //
+
+
+        mMap.setMyLocationEnabled(true);
+
+    }
+
+    LocationCallback mLocationCallback = new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            for(Location location : locationResult.getLocations()){
+                if(getApplicationContext()!=null){
+
+                    if(!customerId.equals("") && lastLocation!=null && location != null){
+                        rideDistance += lastLocation.distanceTo(location)/1000;
+                    }
+                    lastLocation = location;
+
+
+                    LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+                    String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                    DatabaseReference refAvailable = FirebaseDatabase.getInstance().getReference("driversAvailable");
+                    DatabaseReference refWorking = FirebaseDatabase.getInstance().getReference("driversWorking");
+//                    GeoFire geoFireAvailable = new GeoFire(refAvailable);
+//                    GeoFire geoFireWorking = new GeoFire(refWorking);
+//
+//                    switch (customerId){
+//                        case "":
+//                            geoFireWorking.removeLocation(userId);
+//                            geoFireAvailable.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()));
+//                            break;
+//
+//                        default:
+//                            geoFireAvailable.removeLocation(userId);
+//                            geoFireWorking.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()));
+//                            break;
+//                    }
+                }
+            }
+        }
+    };
+
+
+
+    private void checkLocationPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                new AlertDialog.Builder(this)
+                        .setTitle("give permission")
+                        .setMessage("give permission message")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MapsTaiXeActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                            }
+                        })
+                        .create()
+                        .show();
+            }
+            else{
+                ActivityCompat.requestPermissions(MapsTaiXeActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        lastLocation = location;
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
     }
 }
